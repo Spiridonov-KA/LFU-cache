195e04a9200f9484e772b7dd6d5cf20268d1f265
Kirill Spriridonov <vo-ro@list.ru>
2024-09-25 11:01:25 +0300

    creat tests for perfect cache

--- main.cpp
+++ main.cpp
@@ -12,42 +12,45 @@
 // slow get page imitation
 int slow_get_page_int(int key) { return key; }
 
+void run_LFU_cache(std::vector<int> &queries, size_t m) {
+  int hits = 0;
+  int n = queries.size();
+  caches::cache_t<int> c{m};
+  for (int i = 0; i < n; ++i) {
+    int q;
+    q = queries[i];
+    if (c.lookup_update(q, slow_get_page_int)) {
+      hits += 1;
+  #ifdef DEBUG_INFO
+      std::cout << "was hit in" << std::endl;
+  #endif
+    }
+#ifdef DEBUG_INFO
+  std::cout << q << ' ' << i << '\n';
+  c.print_cache();
+#endif
+  }
+  std::cout << hits << std::endl;
+}
+
 int main() {
   int hits = 0;
   int n;
   size_t m;
 
   std::cin >> m >> n;
-  // assert(std::cin.good());
-  caches::cache_t<int> c{m};
   std::vector<int> queries(n);
 
   for (int i = 0; i < n; ++i) {
     std::cin >> queries[i];
   }
 
-  for (int i = 0; i < n; ++i) {
-    int q;
-    // std::cin >> q;
-    // assert(std::cin.good());
-    q = queries[i];
-    if (c.lookup_update(q, slow_get_page_int)) {
-      hits += 1;
-#ifdef DEBUG_INFO
-      std::cout << "was hit in" << std::endl;
+#ifdef LFU_CACHE_RESULT
+  run_LFU_cache(queries, m);
 #endif
-    }
-#ifdef DEBUG_INFO
-    std::cout << q << ' ' << i << '\n';
-    c.print_cache();
-#endif
-  }
 
-#ifdef PERFECT_CACHE
+#ifdef PERFECT_CACHE_RESULT
   caches::perfect_cache_t<int> p_c{m, queries};
-  std::cout << "Perfect cache hits: " << p_c.count_hits(slow_get_page_int) << '\n';
+  std::cout << p_c.count_hits(slow_get_page_int) << '\n';
 #endif
-
-
-  std::cout << hits << std::endl;
 }
